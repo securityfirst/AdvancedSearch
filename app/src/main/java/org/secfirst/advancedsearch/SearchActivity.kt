c31b5e22463cec71147a82f3bbb45a1f2cdbd3a1
Rok Biderman <rok.biderman@gmail.com>
2018-12-13 01:31:44 -0800

    Add Search results view to search results activity

--- app/src/main/java/org/secfirst/advancedsearch/SearchActivity.kt
+++ app/src/main/java/org/secfirst/advancedsearch/SearchActivity.kt
@@ -3,7 +3,8 @@ package org.secfirst.advancedsearch
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_search.*
+import org.secfirst.advancedsearch.mvp.views.SearchResultView
 import java.util.logging.Logger
 
 class SearchActivity : AppCompatActivity() {
@@ -16,8 +17,7 @@ class SearchActivity : AppCompatActivity() {
         // Verify the action and get the query
         AdvancedSearch.getQueryFromSearchIntent(intent)?.let {
             Logger.getLogger(TAG).info("Searchinng for ${it}")
-            Toast.makeText(this@SearchActivity, "Searchinng for ${it}", Toast.LENGTH_LONG).show()
-
+            (searchResultView as SearchResultView).passIntent(intent)
         }
     }
 
